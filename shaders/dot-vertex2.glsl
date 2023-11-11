0bb501fe4d2c53bd5ba810f7db0c5d1891e7dce9
Andre-John Mas <andrejohn.mas@gmail.com>
2023-11-10 20:30:59 -0500

    initital gh-pages

---
+++ shaders/dot-vertex2.glsl
@@ -0,0 +1,24 @@
+  attribute vec2 aPos;
+  attribute vec4 aColor;
+
+  uniform vec2 u_resolution;
+
+  uniform vec2 u_translation;
+
+  varying vec4 vColor;
+
+  void main() {
+    // Add in the translation.
+    vec2 position = aPos + u_translation;
+    // convert the rectangle from pixels to 0.0 to 1.0
+    vec2 zeroToOne = position / u_resolution;
+
+    // convert from 0->1 to 0->2
+    vec2 zeroToTwo = zeroToOne * 2.0;
+
+    // convert from 0->2 to -1->+1 (clipspace)
+    vec2 clipSpace = zeroToTwo - 1.0;
+
+    gl_Position = vec4(clipSpace*vec2(1,-1), 0, 1);
+    vColor = aColor;
+  }
